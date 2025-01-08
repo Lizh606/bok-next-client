cacb675ec2b3008f82f03b2bc022e56e54675dbb
李泽航 <lizh@dist.com.cn>
2025-01-08 15:55:44 +0800

    fix(post): post页面标题修改用generateMetadata

--- src/app/(app)/posts/[sort]/[id]/page.tsx
+++ src/app/(app)/posts/[sort]/[id]/page.tsx
@@ -8,29 +8,34 @@ let metadata: Metadata = {}
 
 type Props = {
   params: { id: number; sort: string }
-  searchParams: { [key: string]: string | string[] | undefined }
+  // searchParams: { [key: string]: string | string[] | undefined }
+}
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const post = await getPostById(params.id)
+  return {
+    title: post.title
+  }
 }
 
 export default async function Post({ params }: Props) {
   const post = await getPostById(params.id)
-  metadata.title = post.title
   return (
     <div className="mt-20">
-      <h1 className="text-4xl font-bold text-center text-balance mb-4">
+      <h1 className="mb-4 text-balance text-center text-4xl font-bold">
         {post.title}
       </h1>
       <div className="flex w-full items-center justify-center">
-        <div className="flex gap-4 items-center text-default-700">
+        <div className="flex items-center gap-4 text-default-700">
           <WithIconTime
             time={calculateTimeDifference(post.date.toString())}
           ></WithIconTime>
           <span>#{post.tag}</span>
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-6 relative mt-8">
+      <div className="relative mt-8 grid grid-cols-4 gap-6">
         <MarkDownPage post={post}></MarkDownPage>
       </div>
     </div>
   )
 }
-export { metadata }
+// export { metadata }
