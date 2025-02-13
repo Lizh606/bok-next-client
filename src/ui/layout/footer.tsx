2a26af4fcd520e646dc2abd847ce7f04a4e01511
李泽航 <lizh@dist.com.cn>
2025-02-13 11:00:53 +0800

    chore: 更新icp跳转地址

--- src/ui/layout/footer.tsx
+++ src/ui/layout/footer.tsx
@@ -7,21 +7,18 @@ import Social from "../../components/Social"
 export default function Footer() {
   const { BOK_YEAR, BOK_AUTHOR, BOK_ICP } = process.env
   return (
-    <footer className="relative z-[1] mt-24 border-t py-8 dark:border-[#2B2928] text-default-600 text-xs">
-      <div className="px-[4.5rem] flex items-center justify-center">
+    <footer className="relative z-[1] mt-24 border-t py-8 text-xs text-default-600 dark:border-[#2B2928]">
+      <div className="flex items-center justify-center px-[4.5rem]">
         <div className="flex flex-col gap-4">
           <div className="flex flex-col gap-4">
             <Social></Social>
-            <Link href={"/"} className="flex gap-1 items-center">
-              <Site className="w-5 h-5" />
+            <Link href={"/"} className="flex items-center gap-1">
+              <Site className="h-5 w-5" />
               {BOK_YEAR} • {BOK_AUTHOR}
             </Link>
-            <Link
-              target="_blank"
-              href="https://beian.miit.gov.cn/#/Integrated/index"
-            >
-              <span className="flex gap-1 items-center">
-                <ICP className="w-5 h-5" /> {BOK_ICP}
+            <Link target="_blank" href="https://beian.miit.gov.cn">
+              <span className="flex items-center gap-1">
+                <ICP className="h-5 w-5" /> {BOK_ICP}
               </span>
             </Link>
           </div>
