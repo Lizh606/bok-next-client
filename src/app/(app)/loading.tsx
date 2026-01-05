b5b78d58ec62d741ba409bad0d1a7d88103dd837
lizh <915194732@qq.com>
2026-01-05 16:04:47 +0800

    fix: loading文案修改

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -3,9 +3,9 @@ import { Spinner } from "@heroui/react"
 
 export default function Loading() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="flex h-screen w-full items-center justify-center">
       <Spinner
-        label="心莫急，马上来咯🫵"
+        label="心莫急，马上来咯"
         color="warning"
         labelColor="warning"
         className="animate-bounce"
